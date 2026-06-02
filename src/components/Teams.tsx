6b1ebe70d58aa6d5e9c44848a74dde70e4c0eb98
Dagem Tadesse <43210896+dagemtadesse@users.noreply.github.com>
2026-06-02 16:26:28 +0300

    .

--- src/components/Teams.tsx
+++ src/components/Teams.tsx
@@ -14,7 +14,7 @@ const AdvisoryLeaders = () => {
       image: "/profile-picture/abebe-assefa.jpeg", // Replace with actual image
     },
     {
-      name: "Tekle Mengesha",
+      name: "Tigabu Dange",
       description:
         "Technology Advisor | 16+ years leading project  management and research & development initiatives",
       image: "/profile-picture/tekle-mengesha.jpeg",
